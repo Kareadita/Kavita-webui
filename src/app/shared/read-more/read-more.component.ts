8600d75ee95e3b94aaa5e7a2f694ee2f6fb22ff3
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-04-03 15:16:02 -0500

    Implemented Read more component (Fixes #117)

---
+++ src/app/shared/read-more/read-more.component.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-read-more',
+  templateUrl: './read-more.component.html',
+  styleUrls: ['./read-more.component.scss']
+})
+export class ReadMoreComponent implements OnChanges {
+
+  @Input() text!: string;
+  @Input() maxLength: number = 250;
+  currentText!: string;
+  hideToggle: boolean = true;
+
+  public isCollapsed: boolean = true;
+
+  constructor(private elementRef: ElementRef) {
+
+  }
+  toggleView() {
+      this.isCollapsed = !this.isCollapsed;
+      this.determineView();
+  }
+  determineView() {
+      if (!this.text || this.text.length <= this.maxLength) {
+          this.currentText = this.text;
+          this.isCollapsed = false;
+          this.hideToggle = true;
+          return;
+      }
+      this.hideToggle = false;
+      if (this.isCollapsed === true) {
+        this.currentText = this.text.substring(0, this.maxLength);
+        this.currentText = this.currentText.substr(0, Math.min(this.currentText.length, this.currentText.lastIndexOf(" ")));
+        this.currentText = this.currentText + '…';
+      } else if (this.isCollapsed === false)  {
+          this.currentText = this.text;
+      }
+
+  }
+  ngOnChanges() {
+      this.determineView();
+  }
+}
