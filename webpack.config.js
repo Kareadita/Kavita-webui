7f8c839b88e987a014820e2f346f3d336b862b7e
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-05-05 19:50:05 -0500

    Backed out purge css after testing

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const glob = require('glob');
-const PurgeCSSPlugin = require('purgecss-webpack-plugin');
-
-module.exports = {
-  plugins: [
-    new PurgeCSSPlugin({ paths: glob.sync('./src/**/*.html', { nodir: true }) })
-  ]
-};
