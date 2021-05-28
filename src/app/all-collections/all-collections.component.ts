69de34cb26fba88929b95ee054ac11f61bc6015a
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-05-28 11:27:50 -0500

    Additional RBS check for tags where the tag fragment is invalid or there are no libraries that a user has access to

--- src/app/all-collections/all-collections.component.ts
+++ src/app/all-collections/all-collections.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { CollectionTag } from '../_models/collection-tag';
 import { Pagination } from '../_models/pagination';
 import { Series } from '../_models/series';
@@ -24,7 +25,7 @@ export class AllCollectionsComponent implements OnInit {
   seriesPagination!: Pagination;
 
   constructor(private collectionService: CollectionTagService, private router: Router, 
-    private route: ActivatedRoute, private seriesService: SeriesService) {
+    private route: ActivatedRoute, private seriesService: SeriesService, private toastr: ToastrService) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 
     const routeId = this.route.snapshot.paramMap.get('id');
@@ -32,6 +33,12 @@ export class AllCollectionsComponent implements OnInit {
       this.collectionTagId = parseInt(routeId, 10);
       this.collectionService.allTags().subscribe(tags => {
         this.collections = tags;
+        const matchingTags = this.collections.filter(t => t.id === this.collectionTagId);
+        if (matchingTags.length === 0) {
+          this.toastr.error('You don\'t have access to any libraries this tag belongs to or this tag is invalid');
+          this.router.navigate(['collections']);
+          return;
+        }
         this.collectionTagName = tags.filter(item => item.id === this.collectionTagId)[0].title;
       });
     }
