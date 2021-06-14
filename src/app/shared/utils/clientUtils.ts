4527f0c3aadafeca118bab5bbeb4ede60b3249ab
Leonardo Dias <contato.leonardod@yahoo.com>
2021-06-14 12:44:33 -0300

    feat: add client anonymous data collection

---
+++ src/app/shared/utils/clientUtils.ts
@@ -0,0 +1,23 @@
+import * as Bowser from "bowser";
+import { version } from '../../../../package.json';
+import { ClientInfo } from "src/app/_models/client-info";
+import { DetailsVersion } from "src/app/_models/details-version";
+
+const getClientInfo = (): ClientInfo => {
+
+    const screenResolution = `${window.screen.width} x ${window.screen.height}`;
+
+    const browser = Bowser.getParser(window.navigator.userAgent);
+
+    const info: ClientInfo = {
+        os: browser.getOS() as DetailsVersion,
+        browser: browser.getBrowser() as DetailsVersion,
+        platformType: browser.getPlatformType(),
+        kavitaUiVersion: version,
+        screenResolution
+    }
+
+    return info;
+}
+
+export { getClientInfo };
